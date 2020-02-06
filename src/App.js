f168ff556cf33d51a5ced96e015cda42593ad829
Jared Tanner <Deraj21@gmail.com>
2020-02-05 21:23:34 -0700

    character sort, add, edit all work

--- src/App.js
+++ src/App.js
@@ -1,17 +1,104 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import ToolBar from './components/Toolbar'
-import CharacterList from './components/CharacterList';
+import Character from './components/Character';
 
-function App() {
-  return (
-    <div className="App">
-      App:
 
-      <ToolBar />
-      <CharacterList />
-    </div>
-  );
+
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      characterData: [
+        {
+          name: "Welby",
+          initiative: 12
+        }
+      ]
+    }
+
+    this.updateCharacter = this.updateCharacter.bind(this)
+    this.onSort = this.onSort.bind(this)
+    this.addCharacter = this.addCharacter.bind(this)
+    this.next = this.next.bind(this)
+  }
+
+  addCharacter(){
+    let newData = [...this.state.characterData]
+    newData.push({
+      name: "",
+      initiative: 1,
+      id: this.state.characterData.length
+    })
+
+    this.setState({
+      characterData: newData
+    })
+  }
+
+  updateCharacter(props){
+    // update character
+    let { id } = props
+    let newData = [...this.state.characterData]
+    let i = newData.findIndex(data => data.id === id)
+    newData[i] = Object.assign({}, newData[i], props)
+
+    // validate
+    let init = newData[i].initiative
+    if (init){ 
+      newData[i].initiative = init > 99 ? 99 : init < 1 ? 1 : init
+    }
+
+    this.setState({
+      characterData: newData
+    })
+  }
+
+  onSort(){
+    let sortedData = [...this.state.characterData]
+    sortedData.sort((a, b) => b.initiative - a.initiative)
+    this.setState({
+      characterData: [...sortedData]
+    })
+  }
+
+  next(){
+    let advanced = [...this.state.characterData]
+    let front = advanced.shift()
+    advanced.push(front)
+
+    this.setState({
+      characterData: advanced
+    })
+  }
+
+  render(){
+    let { characterData } = this.state
+    let characters = characterData.map((c, i) => {
+      return (
+        <Character name={c.name} initiative={c.initiative} key={`character-${c.id}`} id={c.id} updateCharacter={this.updateCharacter}/>
+      )
+    })
+    
+    return (
+      <div className="App">
+        <div className="toolbar">
+          <h2>Turn Order</h2>
+          <div className="btn-box">
+            <button onClick={this.addCharacter}>Add</button>
+            <button onClick={this.onSort}>Sort</button>
+            <button onClick={this.next}>Next Turn</button>
+          </div>
+        </div>
+        
+        {/* Character List */}
+        <div className="characterList">
+          { characters }
+        </div>
+
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,10 +1,49 @@
 import React, { Component } from 'react'
 
 class Character extends Component{
+  constructor(props){
+    super(props)
+
+    this.state = {
+      isEditingName: this.props.id % 2 === 0,
+      isEditingInit: this.props.id % 2 === 0
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(e){
+    let binding = e.target.getAttribute("binding")
+    let newProps = Object.assign({}, this.props)
+    newProps[binding] = e.target.value
+
+    this.props.updateCharacter(newProps)
+  }
+
   render(){
+    let { isEditingName, isEditingInit } = this.state
+    let { name, initiative } = this.props
+
     return (
       <div className="Character">
-        Character
+        {
+          isEditingName
+          ?
+          <input value={name} binding="name" onChange={this.handleChange}
+            className="name-input" maxLength="30"
+          />
+          :
+          <p>{name}</p>
+        }
+        {
+          isEditingInit
+          ?
+          <input value={initiative} binding="initiative" onChange={this.handleChange}
+            type="number" min="1" max="99" className="init-input"
+          />
+          :
+          <p className="init-p">{initiative}</p>
+        }
       </div>
     )
   }
